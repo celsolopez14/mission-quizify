1fc86555e4553ed157f0fd0d873cb513bbf3fbbd
Celso <celopez@my.bridgeport.edu>
2024-03-18 00:07:54 -0400

    Completed tasks 9 and 10. The app displays the questions and after answering it shows correct/incorrect and explains them.

--- tasks/task_9/task_9.py
+++ tasks/task_9/task_9.py
@@ -27,7 +27,8 @@ class QuizManager:
         Note: This initialization method is crucial for setting the foundation of the `QuizManager` class, enabling it to manage the quiz questions effectively. The class will rely on this setup to perform operations such as retrieving specific questions by index and navigating through the quiz.
         """
         ##### YOUR CODE HERE #####
-        pass # Placeholder
+        self.questions = questions
+        self.total_questions = len(questions)
     ##########################################################
 
     def get_question_at_index(self, index: int):
@@ -62,16 +63,21 @@ class QuizManager:
         Note: Ensure that `st.session_state["question_index"]` is initialized before calling this method. This navigation method enhances the user experience by providing fluid access to quiz questions.
         """
         ##### YOUR CODE HERE #####
-        pass  # Placeholder for implementation
+        if "question_index" in st.session_state:
+            question_index = st.session_state["question_index"]
+            st.session_state["question_index"] = (direction + question_index) % self.total_questions
     ##########################################################
 
 
 # Test Generating the Quiz
 if __name__ == "__main__":
     
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '../../authentication.json'
+
+
     embed_config = {
         "model_name": "textembedding-gecko@003",
-        "project": "YOUR-PROJECT-ID-HERE",
+        "project": "gemini-quizify-417419",
         "location": "us-central1"
     }
     
@@ -112,11 +118,11 @@ if __name__ == "__main__":
             
             # Task 9
             ##########################################################
-            quiz_manager = # Use our new QuizManager class
+            quiz_manager = QuizManager(question_bank)
             # Format the question and display
             with st.form("Multiple Choice Question"):
                 ##### YOUR CODE HERE #####
-                index_question = # Use the get_question_at_index method to set the 0th index
+                index_question = quiz_manager.get_question_at_index(0)
                 ##### YOUR CODE HERE #####
                 
                 # Unpack choices for radio
@@ -124,13 +130,16 @@ if __name__ == "__main__":
                 for choice in index_question['choices']: # For loop unpack the data structure
                     ##### YOUR CODE HERE #####
                     # Set the key from the index question 
+                    key = choice["key"]
                     # Set the value from the index question
+                    value = choice["value"]
                     ##### YOUR CODE HERE #####
                     choices.append(f"{key}) {value}")
                 
                 ##### YOUR CODE HERE #####
                 # Display the question onto streamlit
                 ##### YOUR CODE HERE #####
+                st.write(index_question["question"])
                 
                 answer = st.radio( # Display the radio button with the choices
                     'Choose the correct answer',
